0e8f895e57a7ed6f3aff9df0bbea07dabcb62b76
Peter Wilson <pwillie@users.noreply.github.com>
2017-07-13 13:55:52 +1000

    add health check endpoint

--- redirect-to-https.go
+++ redirect-to-https.go
@@ -1,22 +1,32 @@
 package main
+
 import (
-    "net/http"
-    "log"
-    "net"
+	"net/http"
+	"log"
+	"net"
 )
 
-func redirect(w http.ResponseWriter, req *http.Request) {
-    host, _, _ := net.SplitHostPort(req.Host)
+func healthcheck_handler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusOK)
+}
 
-    target := "https://" + host + req.URL.Path
-    if len(req.URL.RawQuery) > 0 {
-        target += "?" + req.URL.RawQuery
-    }
-    log.Printf("redirect to: %s", target)
-    http.Redirect(w, req, target, http.StatusMovedPermanently)
+func redirect_handler(w http.ResponseWriter, req *http.Request) {
+	host, _, err := net.SplitHostPort(req.Host)
+	if err != nil {
+		host = req.Host
+	}
+
+	target := "https://" + host + req.URL.Path
+	if len(req.URL.RawQuery) > 0 {
+		target += "?" + req.URL.RawQuery
+	}
+	log.Printf("redirect to: %s", target)
+	http.Redirect(w, req, target, http.StatusMovedPermanently)
 }
 
 func main() {
-    // redirect every http request to https
-    http.ListenAndServe(":8080", http.HandlerFunc(redirect))
+	// redirect every http request to https
+	http.HandleFunc("/status", healthcheck_handler)
+	http.HandleFunc("/", redirect_handler)
+	http.ListenAndServe(":8080", nil)
 }
